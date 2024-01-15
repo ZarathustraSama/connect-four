45db50e32bfd06970c4a480cd644c14ee3ecd275
ZarathustraSama <mr.gabriel@live.it>
2024-01-15 11:24:15 +0100

    Implemented #check_row_winner

--- lib/grid.rb
+++ lib/grid.rb
@@ -15,7 +15,18 @@ class Grid
   def check_column_winner
     @grid.each do |column|
       column.each_cons(4) do |a|
-        return a[0] if a.uniq.size == 1 && a[0] != EMPTY
+        return a[0] if a.uniq.count == 1 && a[0] != EMPTY
+      end
+    end
+    nil
+  end
+
+  def check_row_winner
+    @grid.each_cons(4) do |group|
+      group.each do |row|
+        row.each_with_index do |cell, i|
+          return cell if [group[0][i], group[1][i], group[2][i], group[3][i]].uniq.count == 1 && cell != EMPTY
+        end
       end
     end
     nil
