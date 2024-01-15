2b4de6b28def73e359a59e45d8b528e2194bec14
ZarathustraSama <mr.gabriel@live.it>
2024-01-15 10:52:56 +0100

    Implemented test for #check_column_winner

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -2,13 +2,27 @@
 
 require_relative '../lib/grid'
 
+HEART = "\u2665"
+DIAMOND = "\u2666"
+EMPTY = nil
+
 describe Grid do
-  describe '#column_win?' do
+  describe '#check_column_winner' do
     context 'when there aren\'t four consecutive pieces' do
       subject(:grid) { described_class.new }
 
-      it 'returns false' do
-        expect(grid.check_win_row).to eql(false)
+      it 'returns nil' do
+        expect(grid.check_column_winner).to eql(nil)
+      end
+    end
+
+    context 'when there are four consecutive pieces' do
+      g = Array.new(6) { Array.new(6) { EMPTY } }
+      g << [HEART, HEART, HEART, HEART, EMPTY, EMPTY]
+      subject(:grid) { described_class.new(g) }
+
+      it 'returns the winner' do
+        expect(grid.check_column_winner).to eql(HEART)
       end
     end
   end
